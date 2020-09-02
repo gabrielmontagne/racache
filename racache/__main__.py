7fa25b421c89f648fffb7e08c3217a54a1d1a899
gabriel montagné láscaris-comneno <gabriel@tibas.london>
2020-09-02 23:17:34 +0200

    generate frame list

--- racache/__main__.py
+++ racache/__main__.py
@@ -1,4 +1,35 @@
+from argparse import ArgumentParser
+from collections import OrderedDict
+from csv import DictReader
+from io import StringIO
+from os.path import abspath, isfile
+from shlex import split
+from subprocess import check_output
+
+alphaToPrestonBlair = {
+    'A': 'MBP',
+    'B': 'etc',
+    'C': 'E',
+    'D': 'AI',
+    'E': 'O',
+    'F': 'U',
+    'G': 'FV',
+    'H': 'L',
+    'X': 'rest'
+}
+
 def main():
-    print('running module racache')
+    parser = ArgumentParser(prog='Racache')
+    parser.add_argument('input')
+    parser.add_argument('-fps', default=10, type=float)
+    args = parser.parse_args()
+    full_path = abspath(args.input)
+    assert isfile(full_path), 'Input file cannot be found'
+    out = check_output(split(f'rhubarb {full_path}')).decode('utf8')
+    fields = DictReader(StringIO(out), dialect='excel-tab', fieldnames=('time', 'viseme'))
+    frames = OrderedDict([(round(float(f['time']) * args.fps), alphaToPrestonBlair.get(f['viseme'])) for f in fields])
+    print('running module racache', frames)
+
 if __name__ == '__main__':
+    print('oks')
     main()
